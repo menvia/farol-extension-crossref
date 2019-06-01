a09425134cd7971a01f148b73ee4dbdcb8275b55
Felipe Plets <felipe.plets@menvia.com>
2019-06-01 08:02:46 -0400

    Fix extension config load

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var extension_kit_1 = require("@farol/extension-kit");
-var farolExtensionConfig = require('../farol-extesion');
+var farolExtensionConfig = require('../farol-extension');
 var crossref = new extension_kit_1.FarolExtension(farolExtensionConfig);
 crossref.register('submission_publish', function (item, config) {
     console.log(item);
