f8d0ad8aff8bd38e4f65d73dee40fea6c3a0dc22
Felipe Plets <felipe.plets@menvia.com>
2019-06-01 00:39:47 -0400

    Fix config loading

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { FarolExtension } from '@farol/extension-kit';
 
-const farolExtensionConfig = require('../farol-extesion');
+const farolExtensionConfig = require('../farol-extension');
 const crossref = new FarolExtension(farolExtensionConfig);
 crossref.register('submission_publish', (item: any, config: any) => {
   console.log(item);
